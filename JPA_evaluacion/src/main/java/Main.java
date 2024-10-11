9619b74e3b883b75402abfbee4a7e7b7f78615ed
CarlosAvalos14 <juancarlosavaloz911@gmail.com>
2024-10-11 12:11:45 -0600

    Update Main.java

--- JPA_evaluacion/src/main/java/Main.java
+++ JPA_evaluacion/src/main/java/Main.java
@@ -4,7 +4,7 @@ import models.Client;
 public class Main {
     public static void main(String[] args) {
         ClientDAO dao = new ClientDAO();
-        Client client1 = new Client("Johan Reyes", "jereyesr@uamv.edu.ni","77051013","8/10/24");
+        Client client1 = new Client("Nelson Lacayo", "nplacayo@uamv.edu.ni","74423431","8/10/24");
         dao.save(client1);
 
     }
